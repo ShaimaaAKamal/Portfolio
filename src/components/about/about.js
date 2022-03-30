01f7666c6416e5a6ca34ec919041d2026d4fc41d
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-03-30 15:50:25 +0200

    Add animation to about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,3 +1,5 @@
+import { faCss3, faHtml5, faJs, faLaravel, faNodeJs, faPhp, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './about.scss';
 const About = () =>{
     return(
@@ -15,6 +17,29 @@ const About = () =>{
             and attractive website designs that incorporate data from databases and services and operate as fast, scalable
            and reliable 
          </p>
+        </div>
+        <div className='stageCube'>
+            <div className='cubeSpinner'>
+                <div className='face1'>
+               <FontAwesomeIcon icon={faPhp} color="#4C578E"/>
+                </div>
+                <div className='face2'>
+               <FontAwesomeIcon icon={faNodeJs} color="#74AA60"/>
+                </div>
+                <div className='face3'>
+               <FontAwesomeIcon icon={faPython} color="#326B98"/>
+                </div>
+                <div className='face4'>
+               <FontAwesomeIcon icon={faReact} color="#5FD3F3"/>
+                </div>
+                <div className='face5'>
+               <FontAwesomeIcon icon={faLaravel} color="#F72B1E"/>
+                </div>
+                <div className='face6'>
+               <FontAwesomeIcon icon={faJs} color="#E9CC30"/>
+                </div>
+                
+            </div>
         </div>
       </div>    );
    }
